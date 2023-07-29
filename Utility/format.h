e8fce82da0105366071ede6308f55be04de988ae
UnboxTheCat <evanhyd2003@gmail.com>
2023-07-29 00:34:04 -0400

    added format library

--- Utility/format.h
+++ Utility/format.h
@@ -3,31 +3,45 @@
 
 #include <string>
 #include <cctype>
+#include <regex>
+#include <type_traits>
 
-template <typename... Args, typename Arg>
-std::string Format(std::string fmt, const Arg& arg, const Args&... args) {
-  std::string formatted;
-  for (int i = 0; i < fmt.size();) {
-    if (fmt[i] == '%') {
-      bool wildcard = false;
-      bool escape = false;
-      int width = 0;
-      for (++i; i < fmt.size(); ++i) {
-        if (isdigit(fmt[i]) && fmt[i] != '0') {
-          ++width;
-        } else if (fmt[i] == 'v') {
-          wildCard = true;
-          break;
-        } else if (fmt[i])
-      }
+/**
+  A custom std::string format function similar to C's printf.
+  A variable's replacement is indicated by the wildcard %?v, where ? is the width.
 
-      if ()
+  Usage:
+  string str = Format("I have %5v marks in %v class", 69, "cs");
+  str -> "I have 69    marks in cs class"
+*/
+template <typename... Args>
+std::string Format(const std::string& fmt, const Args&... args) {
+  using namespace std;
+  static const regex pattern(R"(%([1-9]\d*)?v)");
 
-    } else {
-      formatted += fmt[i];
-      ++i;
+  string parsed;
+  const auto& tokenParser = [&](auto&& self, sregex_iterator result, const auto& arg, const auto&... args) {
+    if (result == sregex_iterator{}) {
+      return;
     }
-  }
+
+    int width = (*result)[1].length() ? stoi((*result)[1]) : 0;
+    string token;
+    if constexpr (requires { to_string(arg); }) {
+      token += to_string(arg);
+    } else {
+      token += arg;
+    }
+    if (token.size() < width) {
+      token.resize(width, ' ');
+    }
+    parsed += result->prefix().str() + token;
+    if constexpr (sizeof...(args) != 0) {
+      self(self, ++result, args...);
+    }
+  };
+  tokenParser(tokenParser, regex_iterator(fmt.begin(), fmt.end(), pattern), args...);
+  return parsed;
 }
 
 #endif
